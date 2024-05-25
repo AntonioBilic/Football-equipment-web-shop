b50824226152b5ebb54370f72e00ea506e9d291e
Antonio Bilic <bilicantonio892gmail.com>
2024-05-25 20:21:22 +0200

    make migrations

--- football_web_shop_app/models.py
+++ football_web_shop_app/models.py
@@ -1,33 +1,121 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from  phonenumber_field.modelfields import PhoneNumberField
 
 
-class Product(models.Model):
-    category_id = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-    name = models.CharField()
-    
+STATUS_CHOICE = (
+    ("process", "Processing"),
+    ("shipped", "Shipped"),
+    ("delivered", "Delivered"),
 
-class Order(models.Model):
+)
+
+RATING = (
+    ( 1, "★☆☆☆☆"), 
+    ( 2, "★★☆☆☆"),
+    ( 3, "★★★☆☆"),
+    ( 4, "★★★★☆"),
+    ( 5, "★★★★★"),
+)
 
 
-
-class OrderItem(models.Model):
-
-
-
-
-
+class User(AbstractUser):
+   first_name = models.CharField(max_length=30)
+   last_name = models.CharField(max_length=30)
+   email = models.EmailField()
+   phone_number = PhoneNumberField()
+   address = models.TextField()
 
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
     slug = models.SlugField() #for user friendly-urls
 
 
+    class Meta:
+        ordering = ('name',) # order in alphabet order
+    
 
-class Meta:
-    ordering = ('name',) # order in alphabet order
+
+    def __str__(self):
+        return self.name
 
 
-def __str__(self):
-    return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    color = models.CharField(max_length=12, null=True, blank=True)
+    material = models.CharField(max_length=50, null=True, blank=True)
+    image = models.ImageField()
+    status = models.BooleanField(default=True)
+    in_stock = models.BooleanField(default=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(null=True, blank=True)
+    #old price specification, tags
+    
+    
+
+    
+    def __str__(self):
+        return self.name
+
+    #def product_image(self):
+        
+    def get_precentage(self):
+        new_price = (self.price / self.old_price) * 100
+        return new_price
+
+
+class ProductImages(models.Model):
+    images = models.ImageField()
+    product = models.ForeignKey(Product, related_name='products', on_delete=models.CASCADE) 
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    paid_staus = models.BooleanField(default=False)
+    order_date = models.DateTimeField(auto_now_add=True)
+    order_status = models.CharField(choices=STATUS_CHOICE, max_length=30, default="processing")
+
+
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='order', on_delete=models.CASCADE )
+    item = models.CharField(max_length=255)
+    image = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField(default=0)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+
+class ProductReview(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    review =  models.TextField()
+    rating = models.IntegerField(choices=RATING, default=None)
+    date =  models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.product.name
+    
+    def get_rating(self):
+        return self.rating
+
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    date =  models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+       return self.product.name
+     
--- football_web_shop_project/settings.py
+++ football_web_shop_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
      # my app
-    'football_web_shop_app'
+    'football_web_shop_app',
+    'phonenumber_field'
+
 ]
 
 MIDDLEWARE = [
@@ -117,11 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS =  [os.path.join(BASE_DIR, 'static')]
 
 
 MEDIA_URL = '/media/'
@@ -131,3 +134,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'football_web_shop_app.User'
