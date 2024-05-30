164d5e4fe3aeb0042e6362b9d7f5ef9acd1daf05
Antonio Bilic <bilicantonio892gmail.com>
2024-05-30 14:54:03 +0200

    user login and logout

--- football_web_shop_app/views.py
+++ football_web_shop_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from .forms import UserRegistrationForm,UserLoginForm
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate,logout
 from django.contrib import messages
 
 def home(request):
@@ -9,30 +9,41 @@ def home(request):
 
 
 #USER REGISTRATION AND LOGIN
-def register(request):
+def register_view(request):
+
+    form = UserRegistrationForm()
+
     if request.method == "POST":
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
-            new_user = form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f"hey {username}, your account was created succesfully")
-            new_user = authenticate(username=form.cleaned_data.get('username'), password = form.cleaned_data.get('password'))
-            login(request, new_user)
-        return redirect('home')
-    else:
-        form = UserRegistrationForm()
-
+            form.save()
+            return redirect('login')
+         
     data = {
-        'form': form,
+        'form' : form
     }
-
-    return render(request, 'account/register.html', data)
-
-
-def login(request):
-    if request.user.is_authenticated:
-        return render ('home')
     
+    return render(request,'account/register.html', data)
+
+
+def login_view(request):
+
     if request.method == 'POST':
+
         username = request.POST.get('username')
         password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username or Password are incorrect')
+
+    return render(request,'account/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
